e559e531855bb5a72004006219255eba98e173bf
Hazel Seanor <avellana1111@gmail.com>
2017-10-22 23:20:57 +0100

    Changed theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,16 +19,21 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+#Set theme
+
+THEME = "pelican-hyde"
+
 # Blogroll
+
+"""
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = (('Twitter', 'https://twitter.com/alarisilver'))
+"""
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
